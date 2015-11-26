4163d901544dfafd947b27a81cf1003adb17c001
Richard Jia <info@xiumu.org>
2015-11-26 16:13:24 +0800

    upload

---
+++ update SWF(NoAds).bat
@@ -0,0 +1,25 @@
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/loader.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/player.swf
+
+::wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/ku6.swf
+::wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/ku6_out.swf
+
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/iqiyi.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/iqiyi5.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/iqiyi_out.swf
+
+::wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/pplive.swf
+::wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/pplive_live.swf
+
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/pps.swf
+
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/tudou.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/sp.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/olc_8.swf
+
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/letv.swf
+
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/sohu.swf
+wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/sohu_live.swf
+
+::wget.exe --no-check-certificate -N https://bitbucket.org/kafan15536900/haoutil/raw/master/player/testmod/adchinaasp.swf
